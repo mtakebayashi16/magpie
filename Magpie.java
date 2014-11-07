630d0581fc67dae8981a3cd5d078a01a596845ea
mtakebayashi16 <mtakebayashi16@punahou.edu>
2014-11-06 14:01:14 -1000

    Activity 3
    finished activity 3

--- Magpie.java
+++ Magpie.java
@@ -10,73 +10,149 @@
  * @author Laurie White
  * @version April 2012
  */
-public class Magpie2
+public class Magpie
 {
- /**
-  * Get a default greeting  
-  * @return a greeting
-  */
- public String getGreeting()
- {
-  return "Hello, let's talk.";
- }
- 
- /**
-  * Gives a response to a user statement
-  * 
-  * @param statement
-  *            the user statement
-  * @return a response based on the rules given
-  */
- public String getResponse(String statement)
- {
-  String response = "";
-  if (statement.indexOf("no") >= 0)
+  /**
+   * Get a default greeting  
+   * @return a greeting
+   */
+  public String getGreeting()
   {
-   response = "Why so negative?";
+    return "Hello, let's talk.";
   }
-  else if (statement.indexOf("mother") >= 0
-    || statement.indexOf("father") >= 0
-    || statement.indexOf("sister") >= 0
-    || statement.indexOf("brother") >= 0)
-  {
-   response = "Tell me more about your family.";
-  }
-  else
-  {
-   response = getRandomResponse();
-  }
-  return response;
- }
-
- /**
-  * Pick a default response to use if nothing else fits.
-  * @return a non-committal string
-  */
- private String getRandomResponse()
- {
-  final int NUMBER_OF_RESPONSES = 4;
-  double r = Math.random();
-  int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
-  String response = "";
   
-  if (whichResponse == 0)
-  {
-   response = "Interesting, tell me more.";
+  /**
+   * Gives a response to a user statement
+   * 
+   * @param statement
+   *            the user statement
+   * @return a response based on the rules given
+   */
+  public String getResponse(String statement){
+    String response = "";
+    if (statement.trim().length() == 0)
+    {
+      response = "Say something, please.";
+    }
+    else if (findKeyword(statement,"hello") >= 0){
+      response = "hello!";
+    }
+    else if (findKeyword(statement,"yes") >= 0){
+      response = "That's cool.";
+    }
+    else if (findKeyword(statement,"math") >= 0
+               || findKeyword(statement,"addition") >= 0
+               || findKeyword(statement,"subtraction") >= 0){
+      response = "I don't like math.";
+    }              
+    else if (findKeyword(statement,"no") >= 0){
+      response = "Why so negative?";
+    }
+    else if (findKeyword(statement,"dog") >= 0
+               || findKeyword(statement,"cat") >= 0){
+      response = "Tell me more about your pets.";
+    }
+    else if (findKeyword(statement,"mother") >= 0
+               || findKeyword(statement, "father") >= 0
+               || findKeyword(statement, "sister") >= 0
+               || findKeyword(statement,"brother") >= 0){
+      response = "Tell me more about your family.";
+    }
+    else if (findKeyword(statement,"Mr. Kiang") >= 0
+               || findKeyword(statement,"Mr. Landgraf") >= 0){
+      response = "He sounds like a good teacher.";
+    }
+    else{
+      response = getRandomResponse();
+    }
+    return response;
   }
-  else if (whichResponse == 1)
+  
+  /**
+   * Pick a default response to use if nothing else fits.
+   * @return a non-committal string
+   */
+  private String getRandomResponse()
   {
-   response = "Hmmm.";
+    final int NUMBER_OF_RESPONSES = 6;
+    double r = Math.random();
+    int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+    String response = "";
+    
+    if (whichResponse == 0){
+      response = "Interesting, tell me more.";
+    }
+    else if (whichResponse == 1){
+      response = "Hmmm.";
+    }
+    else if (whichResponse == 2){
+      response = "Do you really think so?";
+    }
+    else if (whichResponse == 3){
+      response = "You don't say.";
+    }
+    else if (whichResponse == 4){
+      response = "Really?";
+    }
+    else if (whichResponse == 5){
+      response = "That's cool.";
+    }
+    
+    return response;
   }
-  else if (whichResponse == 2)
+  private int findKeyword(String statement, String goal,
+                          int startPos)
   {
-   response = "Do you really think so?";
+    String phrase = statement.trim();
+    // The only change to incorporate the startPos is in
+    // the line below
+    int psn = phrase.toLowerCase().indexOf(
+                                           goal.toLowerCase(), startPos);
+    
+    // Refinement--make sure the goal isn't part of a
+    // word
+    while (psn >= 0)
+    {
+      // Find the string of length 1 before and after
+      // the word
+      String before = " ", after = " ";
+      if (psn > 0)
+      {
+        before = phrase.substring(psn - 1, psn)
+          .toLowerCase();
+      }
+      if (psn + goal.length() < phrase.length())
+      {
+        after = phrase.substring(
+                                 psn + goal.length(),
+                                 psn + goal.length() + 1)
+          .toLowerCase();
+      }
+      
+      // If before and after aren't letters, we've
+      // found the word
+      if (((before.compareTo("a") < 0) || (before
+                                             .compareTo("z") > 0)) // before is not a
+            // letter
+            && ((after.compareTo("a") < 0) || (after
+                                                 .compareTo("z") > 0)))
+      {
+        return psn;
+      }
+      
+      // The last position didn't work, so let's find
+      // the next, if there is one.
+      psn = phrase.indexOf(goal.toLowerCase(),
+                           psn + 1);
+      
+    }
+    
+    return -1;
   }
-  else if (whichResponse == 3)
-  {
-   response = "You don't say.";
-  }
-
-  return response;
+  
+   private int findKeyword(String statement, String goal)
+ {
+  return findKeyword(statement, goal, 0);
  }
+  
 }
